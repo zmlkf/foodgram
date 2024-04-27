ea64eff1aae3e1c3d8bb551c6881318e0ebbfb0d
Roman Zemliakov <zemliakovroman97@gmail.com>
2024-04-27 20:08:46 +0200

    version3

--- backend/recipes/validators.py
+++ backend/recipes/validators.py
@@ -10,5 +10,6 @@ def validate_username(username):
         raise ValidationError(constants.INVALID_USERNAME)
     invalid_chars = re.sub(r'[\w.@+-]', '', username)
     if invalid_chars:
-        raise ValidationError(f'{constants.INVALID_CHARACTERS}: {invalid_chars}')
+        raise ValidationError(
+            f'{constants.INVALID_CHARACTERS}: {invalid_chars}')
     return username
